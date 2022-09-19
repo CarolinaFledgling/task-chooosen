3da106dea1e0b8fb841ba1801526295a4840b67f
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-09-19 15:16:59 +0200

    split into a single CardProfile component for displying one card, working on types in app, working on RWD

--- components/CardProfile/CardProfile.tsx
+++ components/CardProfile/CardProfile.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Box, CardActionArea, CardActions } from "@mui/material";
 
 interface DataCard {
   id: number;
@@ -8,14 +13,139 @@ interface DataCard {
   heading: string;
   ratting: number;
   numberEmission: number;
+  isKilogram: boolean;
 }
 
-type DataCardsArray = DataCard[];
+interface CardProfileProps {
+  card: DataCard;
+}
 
-export const CardProfile = () => {
+function CardProfile({ card }: CardProfileProps): JSX.Element {
+  const { title, url, heading, numberEmission, isKilogram } = card;
+
   return (
-    <div>
-      <p></p>
-    </div>
+    <>
+      <Card
+        sx={{
+          width: 345,
+          border: "1px solid red",
+          padding: 1,
+          borderRadius: 2,
+        }}
+      >
+        <CardActionArea
+          sx={{
+            border: "1px solid blue",
+            borderRadius: 2,
+            backgroundImage: `url(${url})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <Box
+            component="div"
+            sx={{
+              width: "100%",
+              height: "100%",
+              backgroundColor: "rgba(0,0,255,0.2)",
+              p: 3,
+            }}
+          >
+            <CardContent sx={{ p: 2 }}>
+              <Box sx={{ pl: 2, pr: 2 }}>
+                <Typography
+                  gutterBottom
+                  component="h3"
+                  sx={{
+                    fontSize: 24,
+                    fontWeight: "bold",
+                    textAlign: "center",
+                    color: "white",
+                  }}
+                >
+                  {title}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  component="p"
+                  sx={{
+                    fontSize: 16,
+                    color: "white",
+                    textAlign: "center",
+                  }}
+                >
+                  {heading}
+                </Typography>
+              </Box>
+              <Box
+                sx={{
+                  p: 1,
+                  mt: 1,
+                  mb: 1,
+                  backgroundColor: "#333",
+                  borderRadius: "16px",
+                  display: "flex",
+                  justifyContent: "space-around",
+                  alignItems: "center",
+                }}
+                component="div"
+              >
+                <Typography
+                  gutterBottom
+                  component="h3"
+                  sx={{
+                    fontSize: 16,
+                    fontWeight: "bold",
+                    textAlign: "center",
+                    color: "white",
+                    m: 0,
+                  }}
+                >
+                  {card.title}
+                </Typography>
+                {isKilogram ? (
+                  <Typography
+                    gutterBottom
+                    component="h3"
+                    sx={{
+                      fontSize: 16,
+                      fontWeight: "bold",
+                      textAlign: "center",
+                      color: "white",
+                      m: 0,
+                    }}
+                  >
+                    {numberEmission}kg CO2
+                  </Typography>
+                ) : (
+                  <Typography
+                    gutterBottom
+                    component="h3"
+                    sx={{
+                      fontSize: 16,
+                      fontWeight: "bold",
+                      textAlign: "center",
+                      color: "white",
+                      m: 0,
+                    }}
+                  >
+                    {numberEmission}t CO2
+                  </Typography>
+                )}
+              </Box>
+            </CardContent>
+            <CardActions>
+              
+            </CardActions>
+          </Box>
+        </CardActionArea>
+      </Card>
+    </>
   );
-};
+}
+
+export default CardProfile;
