72b669fef0458c1292cda7127b9f1536f2df51df
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-09-19 13:19:47 +0200

    added material ui library , started working on style in one card

--- components/CardProfile/CardProfile.tsx
+++ components/CardProfile/CardProfile.tsx
@@ -1,1 +1,21 @@
+import React from "react";
 
+interface DataCard {
+  id: number;
+  name: string;
+  title: string;
+  url: string;
+  heading: string;
+  ratting: number;
+  numberEmission: number;
+}
+
+type DataCardsArray = DataCard[];
+
+export const CardProfile = () => {
+  return (
+    <div>
+      <p></p>
+    </div>
+  );
+};
